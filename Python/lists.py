cdd56c597f0a3fc8af4909ba987381ccc59df827
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-09-20 12:00:11 +0500

    error handling

--- Python/lists.py
+++ Python/lists.py
@@ -1,54 +1,91 @@
-# # # # myList = [1,"mystring",1, True,{"a": "myvalue"},1, [1,2,3],1]
+# # # # # # # # # myList = [1,"mystring",1, True,{"a": "myvalue"},1, [1,2,3],1]
 
 
 
-# # # # # print(myList)
+# # # # # # # # # # print(myList)
 
-# # # # for item in myList:
-# # # #     print(item)
+# # # # # # # # # for item in myList:
+# # # # # # # # #     print(item)
     
     
-# # # def convert_seconds(seconds):
-# # #     minutes = seconds // 60
-# # #     remaining_seconds = seconds % 60
-# # #     hours = minutes // 60
-# # #     remaining_minutes = minutes % 60
-# # #     days = hours // 24
-# # #     remaining_hours = hours % 24
+# # # # # # # # def convert_seconds(seconds):
+# # # # # # # #     minutes = seconds // 60
+# # # # # # # #     remaining_seconds = seconds % 60
+# # # # # # # #     hours = minutes // 60
+# # # # # # # #     remaining_minutes = minutes % 60
+# # # # # # # #     days = hours // 24
+# # # # # # # #     remaining_hours = hours % 24
     
-# # #     return remaining_hours,hours, remaining_minutes, remaining_seconds, days
+# # # # # # # #     return remaining_hours,hours, remaining_minutes, remaining_seconds, days
 
-# # # hours, minutes, seconds, days, r_hours = convert_seconds(100000)
+# # # # # # # # hours, minutes, seconds, days, r_hours = convert_seconds(100000)
 
-# # # print(r_hours)
+# # # # # # # # print(r_hours)
 
-# # animals =["cat", "dog", "rabbit", "hamster", "parrot", "fish"]
+# # # # # # # animals =["cat", "dog", "rabbit", "hamster", "parrot", "fish"]
 
-# # chars = 0
-# # for animal in animals:
-# #     char=0
-# #     for letter in animal:
+# # # # # # # chars = 0
+# # # # # # # for animal in animals:
+# # # # # # #     char=0
+# # # # # # #     for letter in animal:
         
-# #         char += len(letter)
-# #     print(animal,len(animal))    
+# # # # # # #         char += len(letter)
+# # # # # # #     print(animal,len(animal))    
     
-# # print(chars)
+# # # # # # # print(chars)
 
 
-# tasks = ["eat", "sleep", "code", "repeat"]
-# b = "laundry"
+# # # # # # tasks = ["eat", "sleep", "code", "repeat"]
+# # # # # # b = "laundry"
 
-# obj1= enumerate(tasks,1)
-# obj2=enumerate(b,2)
+# # # # # # obj1= enumerate(tasks,1)
+# # # # # # obj2=enumerate(b,2)
 
-# print((obj1))
-# print(list(obj2))
+# # # # # # print((obj1))
+# # # # # # print(list(obj2))
 
-def skip_elements(elements):
-    result = []
-    for index, element in enumerate(elements):
-        if index % 2 != 0:
-            result.append(element)
-    return result
+# # # # # def skip_elements(elements):
+# # # # #     result = []
+# # # # #     for index, element in enumerate(elements):
+# # # # #         if index % 2 != 0:
+# # # # #             result.append(element)
+# # # # #     return result
 
-print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))
+# # # # # print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))
+
+# # # # multiples = []
+# # # # for x in range(1, 11):
+# # # #     multiples.append(x * 7)
+# # # # print(multiples)
+
+
+# # # # languages = ["Python", "Perl", "Ruby", "Java", "C", "Haskell", "Erlang"]
+
+# # # # lengths = [ len(language) for language in languages ]
+
+# # # # print(lengths)
+
+# # # z = [x * x for x in range(1, 11) if x % 2 == 0]
+# # # print(z)
+
+# # file_counts= {"jpg": 10, "txt": 14, "csv": 2, "py": 23}
+
+# # for extension in file_counts.values():
+# #     print(extension)
+
+
+# def division(a,b):
+#     try:
+#         return a / b
+#     except ZeroDivisionError as e:
+#         print("Error:", e)
+#         # print(e.__class__.__name__)
+#         return None
+# division(5,0)   
+
+try:
+    myList = [1, 2, 3]
+    print(myList[3])
+except Exception as e:
+    # print("Error:", e)
+    print(e.__class__.__name__)
